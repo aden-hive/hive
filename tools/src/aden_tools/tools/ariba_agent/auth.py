c13fba2d5a144af1ef9e841b2dd68f78855765a0
JYOTHIR RAGHAVALU BHOGI <113814770+jyothir-369@users.noreply.github.com>
2026-04-18 23:41:39 +0530

    Update auth.py

--- tools/src/aden_tools/tools/ariba_agent/auth.py
+++ tools/src/aden_tools/tools/ariba_agent/auth.py
@@ -1,1 +1,32 @@
 
+import httpx
+import time
+
+
+class OAuth2Client:
+    def __init__(self, client_id, client_secret, token_url):
+        self.client_id = client_id
+        self.client_secret = client_secret
+        self.token_url = token_url
+        self.token = None
+        self.expiry = 0
+
+    async def get_token_async(self):
+        if self.token and time.time() < self.expiry:
+            return self.token
+
+        async with httpx.AsyncClient() as client:
+            response = await client.post(
+                self.token_url,
+                data={
+                    "grant_type": "client_credentials",
+                    "client_id": self.client_id,
+                    "client_secret": self.client_secret,
+                },
+            )
+
+        data = response.json()
+        self.token = data["access_token"]
+        self.expiry = time.time() + data["expires_in"] - 60
+
+        return self.token
